0dba971124d7c75a3626271735be7652daaf83f3
tiwarishubham635 <59199353+tiwarishubham635@users.noreply.github.com>
2020-08-03 13:14:53 +0530

    Update Assignment+4.py

--- Assignment+4.py
+++ Assignment+4.py
@@ -1,12 +1,4 @@
 
-# coding: utf-8
-
-# ---
-# 
-# _You are currently looking at **version 1.1** of this notebook. To download notebooks and datafiles, as well as get help on Jupyter notebooks in the Coursera platform, visit the [Jupyter Notebook FAQ](https://www.coursera.org/learn/python-data-analysis/resources/0dhYG) course resource._
-# 
-# ---
-
 # In[1]:
 
 
@@ -14,24 +6,6 @@ import pandas as pd
 import numpy as np
 from scipy.stats import ttest_ind
 
-
-# # Assignment 4 - Hypothesis Testing
-# This assignment requires more individual learning than previous assignments - you are encouraged to check out the [pandas documentation](http://pandas.pydata.org/pandas-docs/stable/) to find functions or methods you might not have used yet, or ask questions on [Stack Overflow](http://stackoverflow.com/) and tag them as pandas and python related. And of course, the discussion forums are open for interaction with your peers and the course staff.
-# 
-# Definitions:
-# * A _quarter_ is a specific three month period, Q1 is January through March, Q2 is April through June, Q3 is July through September, Q4 is October through December.
-# * A _recession_ is defined as starting with two consecutive quarters of GDP decline, and ending with two consecutive quarters of GDP growth.
-# * A _recession bottom_ is the quarter within a recession which had the lowest GDP.
-# * A _university town_ is a city which has a high percentage of university students compared to the total population of the city.
-# 
-# **Hypothesis**: University towns have their mean housing prices less effected by recessions. Run a t-test to compare the ratio of the mean price of houses in university towns the quarter before the recession starts compared to the recession bottom. (`price_ratio=quarter_before_recession/recession_bottom`)
-# 
-# The following data files are available for this assignment:
-# * From the [Zillow research data site](http://www.zillow.com/research/data/) there is housing data for the United States. In particular the datafile for [all homes at a city level](http://files.zillowstatic.com/research/public/City/City_Zhvi_AllHomes.csv), ```City_Zhvi_AllHomes.csv```, has median home sale prices at a fine grained level.
-# * From the Wikipedia page on college towns is a list of [university towns in the United States](https://en.wikipedia.org/wiki/List_of_college_towns#College_towns_in_the_United_States) which has been copy and pasted into the file ```university_towns.txt```.
-# * From Bureau of Economic Analysis, US Department of Commerce, the [GDP over time](http://www.bea.gov/national/index.htm#gdp) of the United States in current dollars (use the chained value in 2009 dollars), in quarterly intervals, in the file ```gdplev.xls```. For this assignment, only look at GDP data from the first quarter of 2000 onward.
-# 
-# Each function in this assignment below is worth 10%, with the exception of ```run_ttest()```, which is worth 50%.
 
 # In[2]:
 
